ef0e535f0bfb47b1fab1e45f0be5ee9112913cea
Abdulmalek-HoM <malekbaitulmal@gmail.com>
2024-02-14 14:15:14 +0200

    Quanta Team Auto

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/test1.java
@@ -0,0 +1,52 @@
+package com.example.meepmeeptesting;
+
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class test1 {
+
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-35, -60, Math.PI / 2))
+                .lineToY(-45)
+                .waitSeconds(2)
+                .splineTo(new Vector2d(-50, -35.5), 1 * Math.PI)
+
+//                .setTangent(0.5)
+//                .splineTo(new Vector2d(40, -35.5), 0)
+
+//                .splineToLinearHeading(new Pose2d(50, -35.5, 1 * Math.PI), 0)
+                .waitSeconds(2)
+//                .setTangent(Math.PI)
+//                .splineToConstantHeading(new Vector2d(-10, -10), Math.PI / 2)
+                .strafeTo(new Vector2d(-50, -10))
+//                .splineToLinearHeading(new Pose2d(20, -10, 0), 0)
+                .strafeTo(new Vector2d(20, -10))
+                .splineToLinearHeading(new Pose2d(50, -35.5, 2 * Math.PI), -0.1)
+
+//                .splineToLinearHeading(new Pose2d(50, -35.5, 2 * Math.PI), 3)
+//                .strafeTo(new Vector2d(40, -35.5))
+//                                .turn(Math.toRadians(180))
+//                            .lineToY(-61.37)
+//                            .setTangent(1)
+
+
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testingTrajectories/forward_turn_forwardTest.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.testingTrajectories;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
+
+@Autonomous
+public final class forward_turn_forwardTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d beginPose = new Pose2d(-34, -70, Math.PI / 2);
+        if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+            waitForStart();
+
+            Actions.runBlocking(
+                    drive.actionBuilder(beginPose)
+//                            .splineTo(new Vector2d(30, 30), Math.PI / 2)
+//                            .splineTo(new Vector2d(0, 60), Math.PI)
+//                            .lineToX(48)
+//                            .waitSeconds(3)
+////                            .lineToY(-24)
+////                            .waitSeconds(3)
+////                            .splineTo(new Vector2d(36, -34), (2 * Math.PI))
+////                            .waitSeconds(3)
+////                            .lineToXLinearHeading(48, (Math.PI / 4))
+                            ////                           .waitSeconds(3)
+//                            .splineToLinearHeading(new Vector2d(36, 24), Math.PI)
+                            ////                           .splineToLinearHeading(new Pose2d(36, 24, Math.PI), 0)
+
+                            /////////////////////////////////////
+
+                            .lineToY(-45) // head straight to put the pixel
+
+                            .waitSeconds(2)
+                            .splineToLinearHeading(new Pose2d(-50, -35.5, 1 * Math.PI), 0)
+                            .waitSeconds(2)
+                            .strafeTo(new Vector2d(-50, -10))
+//                .splineToLinearHeading(new Pose2d(20, -10, 0), 0)
+                            .strafeTo(new Vector2d(20, -10))
+                            .splineToLinearHeading(new Pose2d(40, -35.5, 2 * Math.PI), 1)
+                            .build());
+
+            Actions.runBlocking(
+                    drive.actionBuilder(new Pose2d(-34, -45, Math.PI / 2))
+                            .splineToLinearHeading(new Pose2d(49.87, -35.5, 2 * Math.PI), 0)
+                            .waitSeconds(1)
+                            .build());
+
+
+
+        }
+
+
+
+
+    }
+    }
+
+
